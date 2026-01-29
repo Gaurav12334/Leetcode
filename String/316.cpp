a93aa337706cef00a3b86f7f4813b850c1336855
Gaurav Shukla <lgs3598@gmail.com>
2026-01-29 21:06:34 +0530

    316

---
+++ String/316.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int>freq(26, 0);
+        vector<bool>visited(26, false);
+        stack<char>st;
+
+        for(char c : s)
+        {
+            freq[c - 'a']++;
+        }
+
+        for(char c : s)
+        {
+            freq[c - 'a']--;
+            if(visited[c - 'a'])
+                continue;
+
+            while(!st.empty() && st.top() > c && freq[st.top() - 'a'] > 0)
+            {
+                visited[st.top() - 'a'] = false;
+                st.pop();
+            }
+            st.push(c);
+            visited[c - 'a'] = true;
+        }
+
+        string ans = "";
+        while(!st.empty())
+        {
+            ans += st.top();
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+       
+        
+    }
+};
